2a266be9d927d85e7d59dce440e74e6d6c5128c2
ValeRT24 <valeria.tapia@ciencias.unam.mx>
2022-05-30 00:35:58 -0500

    Comienzo de las recetas. :)

--- app/src/main/java/fciencias/pdm20222/tarea3/ui/home/HomeFragment.java
+++ app/src/main/java/fciencias/pdm20222/tarea3/ui/home/HomeFragment.java
@@ -9,15 +9,60 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import fciencias.pdm20222.tarea3.R;
+import fciencias.pdm20222.tarea3.adapters.HomeHorAdapter;
+import fciencias.pdm20222.tarea3.adapters.HomeVerAdapter;
 import fciencias.pdm20222.tarea3.databinding.FragmentHomeBinding;
+import fciencias.pdm20222.tarea3.models.HomeHorModel;
+import fciencias.pdm20222.tarea3.models.HomeVerModel;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView homeHorizontalRec, homeVerticalRec;
+    List<HomeHorModel> homeHorModelList;
+    HomeHorAdapter homeHorAdapter;
+
+    /////////////Verical
+    List<HomeVerModel> homeVerModelList;
+    HomeVerAdapter homeVerAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home,container,false);
+
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeVerticalRec = root.findViewById(R.id.home_ver_rec);
+
+        ////////////////Horizontal RecyclerView
+        homeHorModelList = new ArrayList<>();
+
+        homeHorModelList.add(new HomeHorModel(R.drawable.tamales, "Comida Mexicana"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.pasta, "Comida Italiana"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.tartas, "Postre"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.drinks, "Bebidas"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(),homeHorModelList);
+        homeHorizontalRec.setAdapter(homeHorAdapter);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
+
+        ////////////////Vertical RecyclerView
+        homeVerModelList = new ArrayList<>();
+
+        homeVerModelList.add(new HomeVerModel(R.drawable.desayunos,"Tostada de Salmón", "15 min", "5.0" ));
+
+        homeVerAdapter = new HomeVerAdapter(getActivity(),homeVerModelList);
+        homeVerticalRec.setAdapter(homeVerAdapter);
+        homeVerticalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        homeVerticalRec.setHasFixedSize(true);
+        homeVerticalRec.setNestedScrollingEnabled(false);
         return root;
     }
 }
